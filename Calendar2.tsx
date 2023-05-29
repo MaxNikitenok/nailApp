106724b52536fa8a0b2a95a0c424b7a43e5c87c6
Max_Nikitenok <xxx250790@gmail.com>
2023-05-30 02:12:24 +0300

    feat: calender2 render dots, render receptions

--- Calendar2.tsx
+++ Calendar2.tsx
@@ -56,38 +56,50 @@ type Reception = {
 };
 
 type Dot = {
+  concat: any;
   key: string;
   color: string;
-}
+};
 
 type Dots = {
   dots: Dot;
-}
+};
 
 export const Calendar2: React.FC = ({ navigation }: any) => {
   const [receptions, setReceptions] = useState<{
     [key: string]: Reception[];
   }>();
   const [markedDates, setMarkedDates] = useState<{
-    [key: string]: Dot[];
-  }>()
+    [key: string]: { dots: Dot[] };
+  }>();
+
+  const [currentDay, setCurrentDay] = useState([] as Reception[]);
+
+  const [currentDate, setCurrentDate] = useState('');
 
   useEffect(() => {
     const getData = async () => {
       await axios
         .get('http://192.168.0.111:4999/receptions')
         .then(({ data }) => {
-          const reduced = data.reduce(
+          const reducedMarkedDates = data.reduce(
             (
               acc: { [key: string]: Dots },
-              currentItem: { [x: string]: any; date: any }
+              currentItem: { [x: string]: any; date: string }
             ) => {
               const { date, ...item } = currentItem;
 
               if (date in acc) {
-                acc[date] = {dots: [...acc[date].dots, {key: item._id, color: 'green'} as Dot]};
+                acc[date] = {
+                  dots: acc[date].dots.concat({
+                    key: item._id,
+                    color: 'green',
+                  } as Dot),
+                } as unknown as Dots;
               } else {
-                acc[date] = {dots: [{key: item._id, color: 'green'} as Dot]};
+                acc[date] = {
+                  dots: [{ key: item._id, color: 'green' } as Dot],
+                } as unknown as Dots;
               }
 
               return acc;
@@ -95,14 +107,31 @@ export const Calendar2: React.FC = ({ navigation }: any) => {
             {}
           );
 
-          
-          setMarkedDates(reduced);
-          // reduced.map((item: any) => {
-          //   console.log(item)
-          // })
+          setMarkedDates(reducedMarkedDates);
 
-          
-          console.log(markedDates)
+          console.log(markedDates);
+
+          const reduced = data.reduce(
+            (
+              acc: { [key: string]: Reception[] },
+              currentItem: { [x: string]: any; date: any }
+            ) => {
+              const { date, ...item } = currentItem;
+
+              if (date in acc) {
+                acc[date] = [...acc[date], item as Reception];
+              } else {
+                acc[date] = [item as Reception];
+              }
+
+              return acc;
+            },
+            {}
+          );
+
+          setReceptions(reduced);
+
+          console.log(receptions);
         })
         .catch((err) => {
           console.log(err);
@@ -129,10 +158,42 @@ export const Calendar2: React.FC = ({ navigation }: any) => {
   return (
     <SafeAreaView style={styles.safe}>
       <Calendar
+        firstDay={1}
         initialDate={moment().format('YYYY-MM-DD')}
         markingType={'multi-dot'}
         markedDates={markedDates}
+        onDayPress={(e) => {
+          setCurrentDay(receptions ? receptions[e.dateString] : []);
+          setCurrentDate(e.dateString);
+        }}
+        onDayLongPress={(e) => {
+          console.log(e.dateString);
+        }}
       />
+
+      {
+        <Text>{currentDate}</Text>
+      }
+
+      {!currentDay && (
+        <View style={styles.itemContainer}>
+          <Text>
+            пусто
+          </Text>
+        </View>
+      )}
+
+      {currentDay &&
+        currentDay.map((item) => {
+          return (
+            <View style={styles.itemContainer} key={item._id}>
+              <Text>
+                {item.time} {item.name}
+              </Text>
+              <Text>{item.procedures}</Text>
+            </View>
+          );
+        })}
     </SafeAreaView>
   );
 };
